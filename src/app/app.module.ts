77e83f41359ebd6c421614f5d4e28a70a7b0842b
manu <manukhurana10397@gmail.com>
2023-10-31 01:08:42 +0530

    Konductor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,12 @@ import { IndicatorComponent } from './Indicators/indicator/indicator.component'
 import { HomeComponent } from './home/home.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { LeftComponent } from './ActionTray/left/left.component';
-import { RightComponent } from './ActionTray/right/right.component';
-import { ActionsComponent } from './ActionTray/actions/actions.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
-    declarations: [AppComponent, IndicatorComponent, HomeComponent, LeftComponent, RightComponent, ActionsComponent],
+    declarations: [AppComponent, IndicatorComponent, HomeComponent],
     imports: [
         BrowserModule,
         ReactiveFormsModule,
